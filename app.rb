9e59e80f9fdf611acaf067bbd668c5939c3a8d13
Matthew Bunday <zenceph@gmail.com>
2014-09-23 16:00:03 -0400

    Live coded work

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   erb :hello
+end
+
+get '/example_ajax' do
+  "<p>Hello from Sinatra</p>"
+  500
+end
+
+get '/article' do
+  p params
+  if params[:ajax]
+    {title: "War in Syria", content: "ISIS blah blah"}.to_json
+  else
+    erb :hello
+  end
+end
+
+post '/example_ajax' do
+  "HELLO FROM THE POST"
 end
